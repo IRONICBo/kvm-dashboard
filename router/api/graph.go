b32bb3ab8e86a63ba886ca0cea56a089738cb229
Asklv <bbemailspider@163.com>
2023-05-22 09:54:00 +0800

    Get history data.

--- router/api/graph.go
+++ router/api/graph.go
@@ -4,6 +4,7 @@ import (
 	"kvm-dashboard/model"
 	"kvm-dashboard/router/param"
 	"kvm-dashboard/router/ws"
+	"kvm-dashboard/services"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,6 +20,28 @@ func GetHostWorkloadRealtimeStats(c *gin.Context) {
 
 func GetVmWorkloadHistoryStats(c *gin.Context) {
 	// get history data
+	param := &param.VmHistoryArgument{}
+	err := c.ShouldBindQuery(param)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.FailedWithMsg(err.Error()))
+		return
+	}
+
+	if param.Fields == nil {
+		// fix fields
+		param.Fields = []string{
+			"cpu_usage",
+			"mem_usage",
+			"disk_usage",
+			"net_rx_rate",
+			"net_tx_rate",
+		}
+	}
+
+	svc := services.NewService()
+	vmHistorySimpleData := svc.GetLatestSimpleData(param.UUID, param.Fields)
+
+	c.JSON(http.StatusOK, model.SuccessWithData(vmHistorySimpleData))
 }
 
 func GetVmWorkloadRealtimeStats(c *gin.Context) {
--- router/param/vm.go
+++ router/param/vm.go
@@ -3,3 +3,8 @@ package param
 type VmArgument struct {
 	UUID string `json:"uuid" binding:"required"`
 }
+
+type VmHistoryArgument struct {
+	UUID   string   `json:"uuid" binding:"required"`
+	Fields []string `json:"fields" binding:"required"`
+}
