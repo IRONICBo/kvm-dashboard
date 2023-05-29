2be3241778905f203388f5e157f480db09f9d04a
Asklv <bbemailspider@163.com>
2023-05-29 15:49:13 +0800

    Update machine info list.

--- router/api/machine.go
+++ router/api/machine.go
@@ -1,12 +1,19 @@
 package api
 
 import (
+	"kvm-dashboard/model"
 	"kvm-dashboard/services"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetMachineList(c *gin.Context) {
 	svc := services.NewService()
-	svc.GetMachineList()
+	machineInfo, err := svc.GetMachineList()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.FailedWithMsg(err.Error()))
+	}
+
+	c.JSON(http.StatusOK, model.SuccessWithData(machineInfo))
 }
--- services/machine.go
+++ services/machine.go
@@ -3,34 +3,63 @@ package services
 import (
 	"context"
 	"fmt"
-	"kvm-dashboard/rpc/api/threshold"
+	"kvm-dashboard/consts"
+	"kvm-dashboard/model"
+	"kvm-dashboard/rpc/api/info"
 	"kvm-dashboard/rpc/rpc_client"
 	"kvm-dashboard/utils"
+
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-func (svc *Service) GetMachineList() {
-	// &emptypb.Empty{} => empty struct
-	// resp, err := rpc_client.MachineInfoClient.GetMachineInfo(context.Background(), &emptypb.Empty{})
-	req := &threshold.GetDomainThvRequest{
-		Uuid: "a5c655a8-589a-4524-b338-fa9b947a334d",
-	}
-	resp, err := rpc_client.VMTHresholdClient.GetDomainThv(context.Background(), req)
+func (svc *Service) GetMachineList() (*model.MachineInfo, error) {
+	resp, err := rpc_client.MachineInfoClient.GetMachineInfo(context.Background(), &emptypb.Empty{})
 	if err != nil {
 		utils.Log.Error(fmt.Sprintf("Failed to get machine info: %v", err))
+		return model.NewMachineInfo(
+			make([]*model.Host, 0),
+			make([]*model.Vm, 0)), err
 	}
 
-	utils.Log.Info(fmt.Sprintf("resp %v", resp))
-	// resp = resp
-	// respHostList := resp.HostInfo
-	// hostInfoList := make([]*model.HostInfo, 0)
-	// for _, hostInfo := range respHostList {
-	// 	model.NewHost(
-	// 		hostInfo.Name,
-	// 		hostInfo.Uuid,
-	// 		hostInfo.Ip,
-	// 		hostInfo.SshPort,
-	// 		hostInfo.Username,
-	// 	)
-	// 	utils.Log.Info(fmt.Sprintf("hostInfo %v", hostInfo))
-	// }
+	hostInfos := resp.HostInfo
+	vmInfos := resp.VmInfo
+
+	// temp hostinfo map for vm
+	hostInfoMap := make(map[string]*info.HostInfo) // machine info in grpc
+
+	hosts := make([]*model.Host, 0)
+	for _, hostInfo := range hostInfos {
+		host := model.NewHost(
+			hostInfo.Name,
+			hostInfo.Uuid,
+			hostInfo.Ip,
+			hostInfo.SshPort,
+			fmt.Sprintf("qemu+tcp://%s:%s/system", hostInfo.Ip, hostInfo.QemuPort),
+			hostInfo.Username,
+			hostInfo.Passwd,
+		)
+		hosts = append(hosts, host)
+
+		// temp map
+		hostInfoMap[hostInfo.Uuid] = hostInfo
+	}
+
+	vms := make([]*model.Vm, 0)
+	for _, vmInfo := range vmInfos {
+		hostInfo := hostInfoMap[vmInfo.HostId]
+
+		vm := model.NewVm(
+			vmInfo.Name,
+			vmInfo.Uuid,
+			"",
+			fmt.Sprintf("%d", consts.PORT),
+			fmt.Sprintf("qemu+tcp://%s:%s/system", hostInfo.Ip, hostInfo.QemuPort),
+			consts.USERNAME,
+			consts.PASSWORD,
+		)
+		vms = append(vms, vm)
+	}
+
+	machineInfo := model.NewMachineInfo(hosts, vms)
+	return machineInfo, nil
 }
